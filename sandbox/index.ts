b90f981f30e2d0978cf8b10251b5db49691e093e
maheshwari <archit.maheshwari@payufin.com>
2023-02-24 16:29:07 +0530

    Issue with HSET while handling null values

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -1,5 +1,25 @@
 import 'dotenv/config';
 import { client } from '../src/services/redis';
 
-const run = async () => {};
+const run = async () => {
+    await client.hSet('car', {
+        'color': 'red',
+        'type': 'SUV'
+    });
+
+    const car = await client.hGetAll('car');
+
+    console.log(car);
+
+    await client.hSet('house', {
+        'color': 'red',
+        'type': 'flat',
+        'maintenance': {'Jan': 'Y'},
+        'owner': null || '', // This is reqquired to fix null error is toString
+    });
+
+    const house = await client.hGetAll('house');
+
+    console.log(house);
+};
 run();
