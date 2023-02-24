f8de323e31eb2e304c6b99c350fde65e01288c49
maheshwari <archit.maheshwari@payufin.com>
2023-02-25 00:46:04 +0530

    like and unlike using sets

--- src/services/queries/likes.ts
+++ src/services/queries/likes.ts
@@ -1,9 +1,26 @@
-export const userLikesItem = async (itemId: string, userId: string) => {};
+import {client} from '$services/redis'
+import {userLikesKey, itemsKey} from '$services/keys'
+
+export const userLikesItem = async (itemId: string, userId: string) => {
+    return client.sIsMember(userLikesKey(userId), itemId);
+};
 
 export const likedItems = async (userId: string) => {};
 
-export const likeItem = async (itemId: string, userId: string) => {};
+export const likeItem = async (itemId: string, userId: string) => {
+    const inserted = await client.sAdd(userLikesKey(userId), itemId);
+    if(inserted) {
+        await client.hIncrBy(itemsKey(itemId), 'likes', 1);
+    }
+};
 
-export const unlikeItem = async (itemId: string, userId: string) => {};
+export const unlikeItem = async (itemId: string, userId: string) => {
+    const removed = await client.sRem(userLikesKey(userId), itemId);
+
+    if(removed) {
+        await client.hIncrBy(itemsKey(itemId), 'likes', -1);
+    }
+
+};
 
 export const commonLikedItems = async (userOneId: string, userTwoId: string) => {};
