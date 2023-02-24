0b739e77b0223bac2b985e815e45220c096f40a7
maheshwari <archit.maheshwari@payufin.com>
2023-02-25 03:14:11 +0530

    setting views on the product using sorted set-increasing after refresh

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -4,10 +4,12 @@ export const usersKey = (userId: string) => `users#${userId}`;
 
 export const sessionsKey = (sessionId: string) => `sessions#${sessionId}`;
 
-export const itemsKey = (itemId: string) => `items#${itemId}`;
-
 export const usernamesUniqueKey = () => `usernames:unique`;
 
 export const userLikesKey = (userId: string) => `userLikes:${userId}`;
 
 export const usernamesKey = () => `usernames`;
+
+// Items
+export const itemsKey = (itemId: string) => `items#${itemId}`;
+export const itemsByViewsKey = () => `itemsByViews`;
