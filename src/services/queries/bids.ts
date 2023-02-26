a0ba90957989f447a08b53746321bd12feaf6dc8
maheshwari <archit.maheshwari@payufin.com>
2023-02-26 11:52:25 +0530

    create bid, get bid history using lists

--- src/services/queries/bids.ts
+++ src/services/queries/bids.ts
@@ -1,7 +1,40 @@
+import {client} from '$services/redis'
+import {bidHistoryKey} from '$services/keys';
+import { DateTime } from 'luxon';
+import { getItem } from './items';
 import type { CreateBidAttrs, Bid } from '$services/types';
 
-export const createBid = async (attrs: CreateBidAttrs) => {};
+export const createBid = async (attrs: CreateBidAttrs) => {
+
+	const serialized = serializeHistory(
+		attrs.amount, 
+		attrs.createdAt
+	) ;
+	
+	return client.rPush(bidHistoryKey(attrs.itemId), serialized);
+};
 
 export const getBidHistory = async (itemId: string, offset = 0, count = 10): Promise<Bid[]> => {
-	return [];
+	const startIndex = -1 * offset - count ; 
+	const endIndex = -1 - offset;
+
+	const range = await client.lRange(
+		bidHistoryKey(itemId),
+		startIndex,
+		endIndex
+	)
+
+	return range.map((bid)=> deseraizeHistory(bid));
 };
+
+const serializeHistory = (amount: number, createdAt: DateTime) => {
+	return `${amount}: ${createdAt}`;
+}
+
+const deseraizeHistory = (store: string) => {
+	const [amount, createdAt] = store.split(':');
+	return {
+		amount: parseFloat(amount),
+		createdAt: DateTime.fromMillis(parseInt(createdAt))
+	}
+}
