10fe76794e963dcf063e3073d543eb2c040761f1
maheshwari <archit.maheshwari@payufin.com>
2023-02-27 00:31:03 +0530

    withLock to handle concurrency

--- src/services/redis/lock.ts
+++ src/services/redis/lock.ts
@@ -1,4 +1,35 @@
-export const withLock = async () => {};
+import {randomBytes} from 'crypto';
+import { client } from '$services/redis';
+
+export const withLock = async (key: string, cb: () => any) => {
+
+	const retryDelayMs = 100;
+	let retries = 20;
+
+	// Generate a random value to set the value as key
+	const token = randomBytes(6).toString('hex');
+
+	// Create a lock key 
+	const lockKey = `lock-${key}`;
+
+	// Setup retry
+	while(retries>=0){
+		retries--;
+		const acquired = await client.set(lockKey, token, {
+			NX: true
+		});
+
+		if(!acquired){
+			await pause(retryDelayMs);
+			continue;
+		}
+
+		const result = await cb();
+		// Unset the lock
+		await client.del(lockKey);
+		return result;
+	} 
+};
 
 const buildClientProxy = () => {};
 
