eb330970accb4fb71463039a553c2fc89d385596
maheshwari <archit.maheshwari@payufin.com>
2023-02-28 11:18:21 +0530

    preprocessing the data for searching

--- src/services/queries/items/search.ts
+++ src/services/queries/items/search.ts
@@ -1,1 +1,13 @@
-export const searchItems = async (term: string, size: number = 5) => {};
+import { client } from '$services/redis';
+import { deserialize } from './deserialize';
+import { itemsIndexKey, itemsKey } from '$services/keys';
+
+
+export const searchItems = async (term: string, size: number = 5) => {
+    const cleaned = term
+                        .replace(/[a-zA-Z0-9]/g, '')
+                        .trim()
+                        .split(' ')
+                        .map((word) => word ? `%${word}%` : '')
+                        .join(' ')
+};
