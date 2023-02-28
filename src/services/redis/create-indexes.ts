924811261c7c7791c1c0e0b29659327ae4c6c88c
maheshwari <archit.maheshwari@payufin.com>
2023-02-28 11:13:27 +0530

    create index on connection to redis

--- src/services/redis/create-indexes.ts
+++ src/services/redis/create-indexes.ts
@@ -1,21 +1,28 @@
-import { client } from '$services/redis';
 import { SchemaFieldTypes } from 'redis';
+import { client } from './client';
 import { itemsIndexKey, itemsKey } from '$services/keys';
 
 export const createIndexes = async () => {
-    return client.ft.create(
-        itemsIndexKey(),
-        {
-            name: {
-                type: SchemaFieldTypes.TEXT
-            },
-            description: {
-                type: SchemaFieldTypes.TEXT
-            }
-        },
-        {
-            ON: 'HASH',
-            PREFIX: itemsKey('')
-        }
-    )
+	const indexes = await client.ft._list();
+	const exists = indexes.find((index) => index === itemsIndexKey());
+
+	if (exists) {
+		return;
+	}
+
+	return client.ft.create(
+		itemsIndexKey(),
+		{
+			name: {
+				type: SchemaFieldTypes.TEXT
+			},
+			description: {
+				type: SchemaFieldTypes.TEXT
+			}
+		},
+		{
+			ON: 'HASH',
+			PREFIX: itemsKey('')
+		}
+	);
 };
