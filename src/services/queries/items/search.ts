aad232d4ee34a6475c6ed87ad0ab70249c80c8b8
maheshwari <archit.maheshwari@payufin.com>
2023-02-28 12:11:46 +0530

    using cleaned text to search the index

--- src/services/queries/items/search.ts
+++ src/services/queries/items/search.ts
@@ -5,9 +5,32 @@ import { itemsIndexKey, itemsKey } from '$services/keys';
 
 export const searchItems = async (term: string, size: number = 5) => {
     const cleaned = term
-                        .replace(/[a-zA-Z0-9]/g, '')
+                        .replace(/[^a-zA-Z0-9]/g, '')
                         .trim()
                         .split(' ')
                         .map((word) => word ? `%${word}%` : '')
                         .join(' ')
+
+    // Look at cleaned and make sure it is valid
+    if (cleaned === ''){
+        return [];
+    }
+
+    // Use the client to do the search
+    const results = await client.ft.search(
+        itemsIndexKey(),
+        cleaned, {
+            LIMIT: {
+                from: 0,
+                size: size
+            }
+        }
+    );
+
+    // console.log(results);
+
+    // Deserialize and retuen the search results
+
+    return results.documents.map(({id, value}) => deserialize(id, value as any));
+
 };
